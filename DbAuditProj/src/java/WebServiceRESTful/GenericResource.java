32d21528983f0fa09e24ec6996f817b56882de6e
Roberval Silva Bett <RoberUau@10.34.133.224>
2015-05-11 20:00:34 -0300

    Adicionados WebService Rest

---
+++ DbAuditProj/src/java/WebServiceRESTful/GenericResource.java
@@ -0,0 +1,26 @@
+package WebServiceRESTful;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+
+
+
+
+@Stateless
+@ApplicationPath("/api")
+@Path("")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class GenericResource extends Application
+{
+    
+    
+
+}
