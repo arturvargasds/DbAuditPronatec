bbd2ebb5ce8a2d769d8abc0edac51693ace645df
PEDRO-SATC <PEDRO-SATC@PEDRO>
2015-06-04 21:22:47 -0300

    teste de paginas com AngularJS

---
+++ DbAuditProj/web/js/main.js
@@ -0,0 +1,17 @@
+/* global angular */
+
+var app = angular.module('DbAudit_App', ['ngRoute']);
+
+/**
+ * configurando as rotas (Routes)
+ */
+app.config(['$routeProvider', function ($routeProvider) {$routeProvider
+    // Home
+    .when("/", {templateUrl: "views/home.html", controller: "PageCtrl"})
+    // Paginas
+    .when("/contato", {templateUrl: "views/contato.html", controller: "PageCtrl"})
+    .when("/sobre", {templateUrl: "views/sobre.html", controller: "PageCtrl"})
+    
+    // é  como um else 
+    .otherwise("/", {templateUrl: "views/home.html", controller: "PageCtrl"});
+}]);
