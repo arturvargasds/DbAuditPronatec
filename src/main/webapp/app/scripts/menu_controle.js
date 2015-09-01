1a89854830c30cda16e69dcd86c1682393de5d4b
pedrinho <pedrinho@10.32.4.177>
2015-08-31 21:38:11 -0300

    Atualização...no cadastro de cliente

--- src/main/webapp/app/scripts/menu_controle.js
+++ src/main/webapp/app/scripts/menu_controle.js
@@ -1,6 +1,6 @@
 /* global angular */
 
-var app = angular.module('DbAudit_App', ['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
 /**
  * configurando as rotas (Routes)
@@ -9,13 +9,13 @@ app.config(['$routeProvider', function ($routeProvider) {$routeProvider
     // Home
     .when("/", {templateUrl: "views/home.jsp", controller: "HomeCtrl"})
     // Paginas
-    .when("/contato", {templateUrl: "views/contato.jsp", controller: "ContatoCtrl"})
-    .when("/cliente", {templateUrl: "pages/cadastro/cliente.jsp", controller: "PageCtrl"})
-    .when("/exemplo", {templateUrl: "pages/cadastro/exemplo.jsp", controller: "PageCtrl"})
-    .when("/cid_edit", {templateUrl: "cidades_gerencia.jsp", controller: "PageCtrl"})
-    .when("/calendario", {templateUrl: "calendario/index.html", controller: "PageCtrl"})
+  
+    .when("/cliente", {templateUrl: "views/clientes.jsp", controller: "ClienteCtrl"})
+    .when("/bairro", {templateUrl: "views/cadastro/bairro.jsp", controller: "BairroCtrl"})
+    .when("/cidade", {templateUrl: "views/cidade.jsp", controller: "ColabCtrl"})
+   
     
     // é  como um else 
-    .otherwise("/", {templateUrl: "views/  home.html", controller: "PageCtrl"});
+    .otherwise("/", {templateUrl: "views/home.jsp", controller: "HomeCtrl"});
     
 }]);
